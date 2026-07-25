d03392ddd9f04466725fdf449f8987785ede80a3
Manglam Tiwari <manglamtiwari1@gmail.com>
2026-07-25 15:35:40 +0530

    Font and color changed
    Font and color changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Mulish } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const mulish = Mulish({
+  variable: "--font-mulish",
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"]
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +31,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${mulish.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`min-h-full flex flex-col ${mulish.className}`}>{children}</body>
     </html>
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 export default function RegisterPage() {
+
+    function handleRegisterButton(){
+        console.log("Register button clicked");
+
+    }
     return (
-        <main className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <main className="min-h-screen bg-emerald-50 flex items-center justify-center">
             <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
                 <h1 className="text-3xl font-bold text-center mb-6">
                     Register School
@@ -38,7 +45,9 @@ export default function RegisterPage() {
                         />
                     </div>
                     <button
-                        className="w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700"
+                        // className="w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700"
+                        className="w-full bg-emerald-600 text-white py-3 rounded-md hover:bg-emerald-700"
+                        onClick={handleRegisterButton}
                     >
                         Register
                     </button>
